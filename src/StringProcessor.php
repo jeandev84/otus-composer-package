208ebaa1e732c2c51a4c84efa636962ea46fea3f
Jean-Claude <jeanyao@ymail.com>
2023-11-10 01:28:42 +0300

    Add phpdoc

--- src/StringProcessor.php
+++ src/StringProcessor.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 namespace JeanDev\OtusComposerPackage;
 
 
+/**
+ * @StringProcessor
+ *
+ * @author <Jean-Claude> <<jeanyao@ymail.com>>
+ *
+ * @version 1.0.0
+ *
+ * @package JeanDev\OtusComposerPackage
+ *
+ * @copyright 2020 <Jean-Claude>
+ *
+ * @license MIT
+*/
 class StringProcessor
 {
       public function getLength(string $s): int
